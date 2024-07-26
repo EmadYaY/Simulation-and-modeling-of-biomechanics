38c794f7af50f7658c61bbb21021ad64f6392b31
Arganex-Emad <80041099+EmadYaY@users.noreply.github.com>
2024-07-27 00:15:22 +0330

    Update Detect-Main Code.py

--- Detect-Main Code.py
+++ Detect-Main Code.py
@@ -99,7 +99,6 @@ while True:
 			cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
 		cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
 
-	# show the output frame
 	cv2.imshow("Frame", frame)
 	key = cv2.waitKey(1) & 0xFF
 
